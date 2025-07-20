b597f2be81d12e2ab32de3e2f67bcefc52edb027
Mujammil Shaikh1 <mujammilshaikh4091@gmail.com>
2025-07-20 19:39:53 +0530

    exploring middleware and next function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-const express = require("express")
+const express = require('express');
 const app = express();
 
-
-app.use("/", (req, res) => {
-  res.send("hello world");
+app.use("/user", [(req, res, next) => {
+  console.log("user no 1 ");
+  next();
+}, (req, res, next) => {
+  console.log("user no 2");
+  res.send("Request fullfilled");
+}], (req, res) => {
+  console.log("user no 3");
+  res.send("hello world")
 })
 
 app.listen(4091, () => {
-  console.log("Server is running on port 4091...")
+  console.log("Server running on port 4091")
 });
